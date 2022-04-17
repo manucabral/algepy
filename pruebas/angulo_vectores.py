645dee341ff1dad778a3bb86eb14c278256a3780
Manuel Cabral <mcabral@frba.utn.edu.ar>
2022-04-16 21:23:29 -0300

    Delete pruebas directory

--- pruebas/angulo_vectores.py
+++
@@ -1,7 +0,0 @@
-from algepy import Vector
-
-u = Vector(x=1, y=1, z=3)
-v = Vector(x=-1, y=0, z=4)
-
-angle = u.angle(v, degrees=True)
-print('Angulo entre los dos vectores: ', angle)
--- pruebas/producto_escalar.py
+++
@@ -1,8 +0,0 @@
-from algepy import Vector
-
-# Dado dos vectores
-u = Vector(x=-3, y=5, z=8)
-v = Vector(x=1, y=1, z=1)
-
-# Obtenemos el producto escalar
-print(u * v)
