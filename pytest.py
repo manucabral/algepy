4a3e608ee37da488391d431ca29122e0e338f665
Manu <mcabral@frba.utn.edu.ar>
2022-04-19 12:04:03 -0300

    espacio final agregado

--- pytest.py
+++ pytest.py
@@ -1,6 +1,14 @@
-from algepy import Vector
+from algepy import Vector, Plot, Point
 
 a = Vector(x=1, y=2, z=3)
-b = Vector(x=2, y=3, z=4)
+b = Vector(x=0, y=2, z=5)
 
-print(a + b)
+plot = Plot(name='Cross product', projection='3d', range=[-5, 5])
+
+cross = a.cross(b)
+
+plot.add_vector(vector=a, origin=Point(x=0, y=0, z=0), color='red')
+plot.add_vector(vector=b, origin=Point(x=0, y=0, z=0), color='blue')
+plot.add_vector(vector=cross, origin=Point(x=0, y=0, z=0), color='green')
+
+plot.show()
