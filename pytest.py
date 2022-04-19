91eeb3b30b1656e26ecd572ade8febe08828a1f0
Manu <mcabral@frba.utn.edu.ar>
2022-04-19 11:17:16 -0300

    Test mas simple

--- pytest.py
+++ pytest.py
@@ -1,17 +1,6 @@
-from algepy import Vector, Point, Plot
+from algepy import Vector
 
-# Definimos el origen de los vectores
-p = Point(x=1, y=2, z=3)
-
-# Declaramos los vectores
 a = Vector(x=1, y=2, z=3)
 b = Vector(x=2, y=3, z=4)
 
-# Creamos el gráfico y especificamos que será 3d
-plot = Plot(name='Punto', projection='3d')
-
-# Agregamos los vectores y el origen
-plot.add_point(point=p, color='red')
-
-#plot.show()
-plot.save(path='testplotpoint.png')
+print(a + b)
